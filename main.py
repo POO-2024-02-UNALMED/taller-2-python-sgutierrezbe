d8d8eaf76ffe4a8f532a3f74042be9ec511e4c0d
Samuel Gutierrez <sgutierrezbe@unal.edu.co>
2024-11-11 00:08:04 -0500

    add_main

---
+++ main.py
@@ -0,0 +1,57 @@
+class Auto:
+    def __init__(self, modelo, precio, asientos, marca, motor, registro, cantidadCreados):
+        
+        self.modelo = modelo
+        self.precio = precio
+        self.asientos = asientos
+        self.marca = marca
+        self.motor = motor
+        self.registro = registro
+        Auto.cantidadCreados = cantidadCreados
+
+    def cantidadAsientos(self):
+        count = 0
+        for asiento in self.asientos:
+            if isinstance(asiento, Asiento):
+                count += 1
+        return count
+    
+    def verificarIntegridad(self):
+        registros_asientos = []
+        registros_asientos.extend(
+            asiento.registro for asiento in self.asientos if isinstance(asiento, Asiento)
+        )
+        if len(registros_asientos) == 1 and self.registro == self.motor.registro == registros_asientos.pop():
+            return "auto original"
+        else:
+            return "Las piezas no son originales"
+
+
+class Motor:
+    def __init__(self, cilindros, tipo, registro):
+        self.cilindros = cilindros
+        self.tipo = tipo
+        self.registro = registro
+    
+    def CambiarRegistro(self, nuevoRegristro):
+        self.registro = nuevoRegristro
+
+    def asignarTipo(self, nuevoTipo):
+        tipos_permitidos = ["gasolina", "electrico"]
+        if nuevoTipo in tipos_permitidos:
+            self.tipo = nuevoTipo
+
+
+class Asiento:
+    def __init__(self, color, precio, registro):
+        self.color = color
+        self.precio = precio
+        self.registro = registro
+
+    def cambiarColor(self, nuevo_color):
+        colores_permitidos = ["rojo", "verde", "amarillo", "negro", "blanco"]
+        if nuevo_color in colores_permitidos:
+            self.color = nuevo_color
+    
+    
+        
